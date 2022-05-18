0a6066f5cf3830aee6fc4c7cd459377a5422c30f
juan pablo <jpagudelo2000@gmal.com>
2022-05-17 19:15:38 -0500

    coins

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-white">Coin Market</h1>
+
+      <input
+        type="text"
+        class="form-control bg-dark text-light rounded-0 border-0 my-4"
+        placeholder="Search coin"
+        @keyup="searchCoin"
+        v-model="textSeach"
+      />
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th v-for="(title, index) in titles" :key="index">{{ title }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(coin, index) in filteredCoins" :key="index">
+            <td class="text-muted">
+              {{ index + 1 }}
+            </td>
+            <td>
+              <img
+                :src="coin.image"
+                :alt="coin.name"
+                style="width: 2rem"
+                class="me-2"
+              />
+              <span>
+                {{ coin.name }}
+              </span>
+
+              <span class="ms-2 text-uppercase text-muted">
+                {{ coin.symbol }}
+              </span>
+            </td>
+            <td>
+              {{ coin.current_price }}
+            </td>
+            <td
+              :class="
+                coin.price_change_percentage_24h >= 0
+                  ? 'text-success'
+                  : 'text-danger'
+              "
+            >
+              {{
+                new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(coin.price_change_percentage_24h)
+              }}
+              %
+            </td>
+            <td>
+              {{
+                new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(coin.total_volume)
+              }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script >
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      coins: [],
+      filteredCoins: [],
+      titles: ["#", "Coin", "Price", "Price Change", "Volume (24h)"],
+      textSeach: "",
+    };
+  },
+
+  async mounted() {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    this.coins = this.filteredCoins = data;
+  },
+
+  methods: {
+    searchCoin(e) {
+      const search = e.target.value.toLowerCase();
+      this.filteredCoins = this.coins.filter((coin) => {
+        return (
+          coin.name.toLowerCase().includes(search) ||
+          coin.symbol.toLowerCase().includes(search)
+        );
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #33393f !important ;
 }
 </style>
